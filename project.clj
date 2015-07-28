6fafc6d822f2b8b94a77c9a1fcdf2bbb3fff5d15
Stephen Clayton <sc13@sanger.ac.uk>
2015-07-28 17:15:01 +0100

    add options

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/sc13-bioinf/lein-fixtures-sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/tools.cli "0.3.1"]]
   :eval-in-leiningen true)
--- src/leiningen/fixtures_sql.clj
+++ src/leiningen/fixtures_sql.clj
@@ -1,6 +1,26 @@
-(ns leiningen.fixtures-sql)
+(ns leiningen.fixtures-sql
+  :require [clojure.tools.cli :refer [parse-opts]])
+
+(def cli-options
+  [["--config CONFIG" "Properties file configuring datasources from which to generate fixtures"]
+  ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["This is my program. There are many like it, but this one is mine."
+        ""
+        "Usage: program-name [options] action"
+        ""
+        "Options:"
+        options-summary
+        (string/join \newline)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
 
 (defn fixtures-sql
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond (:help options) (exit 0 (usage summary))
+          (exit 1 (usage summary)))))
