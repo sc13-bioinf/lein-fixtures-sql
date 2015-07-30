086bb3c86643c90020f2e7b3f6c99943355e8f6e
Stephen Clayton <sc13@sanger.ac.uk>
2015-07-30 17:02:12 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.sc13-bioinf/lein-fixtures-sql "0.0.2"
+(defproject org.clojars.sc13-bioinf/lein-fixtures-sql "0.0.3"
   :description "Generate sql fixtures from your production databases"
   :url "https://github.com/sc13-bioinf/lein-fixtures-sql"
   :license {:name "Eclipse Public License"
